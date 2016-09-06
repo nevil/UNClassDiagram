3b0011f09e61d5cd57919fa26079eb5cc1fa900e
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2016-09-07 07:03:35 +0900

    Trying

--- UNClassDiagram/main.swift
+++ UNClassDiagram/main.swift
@@ -7,6 +7,14 @@
 //
 
 import Foundation
+import SourceKittenFramework
 
-print("Hello, World!")
+//let structure = Structure(file: File(contents: "struct A { func b() {} }"))
+let structure = Structure(file: File(contents: "struct A: C { func b() {} }"))
+
+print("Structure: \(structure.description)")
+
+for e in structure.dictionary {
+    print("e: \(e)")
+}
 
