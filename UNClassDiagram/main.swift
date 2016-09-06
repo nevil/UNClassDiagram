18ef5a75d19add4e1bc1c58fea366b4c184c8b7e
Anders Hasselqvist <anders.hasselqvist@gmail.com>
2016-09-06 23:14:48 +0900

    Base

---
+++ UNClassDiagram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UNClassDiagram
+//
+//  Created by Anders Hasselqvist on 2016/09/06.
+//  Copyright © 2016 Anders Hasselqvist. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
